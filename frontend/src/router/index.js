5ec034259088adf06088e2b402bcf0c36151bee9
nurizzatie <nurizzati.jsl@gmail.com>
2025-07-02 19:26:42 +0800

    Edit redirect routes path

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,17 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Login from '@/views/Login.vue';
 import StudentDashboard from '@/views/student/StudentDashboard.vue';
-// import LecturerDashboard from '@/views/lecturer/LecturerDashboard.vue';
-// import AdvisorDashboard from '@/views/advisor/AdvisorDashboard.vue';
-// import AdminDashboard from '@/views/admin/AdminDashboard.vue';
+import LecturerDashboard from '@/views/lecturer/LecturerDashboard.vue';
+import AdvisorDashboard from '@/views/advisor/AdvisorDashboard.vue';
+import AdminDashboard from '@/views/admin/AdminDashboard.vue';
+import LecturerProfile from '@/views/lecturer/LecturerProfile.vue';
 
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: Login },
-  { path: '/dashboard/student', component: StudentDashboard },
-  // { path: '/dashboard/lecturer', component: LecturerDashboard },
-  // { path: '/dashboard/advisor', component: AdvisorDashboard },
-  // { path: '/dashboard/admin', component: AdminDashboard }
+  { path: '/student/dashboard', component: StudentDashboard },
+  { path: '/lecturer/dashboard', component: LecturerDashboard },
+  { path: '/advisor/dashboard', component: AdvisorDashboard },
+  { path: '/admin/dashboard', component: AdminDashboard },
+  { path: '/lecturer/profile', component: LecturerProfile }
 ];
 
 const router = createRouter({
