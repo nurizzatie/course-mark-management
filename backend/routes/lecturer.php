e504bbb453100c861971ef8e510020528abb4a25
nurizzatie <nurizzati.jsl@gmail.com>
2025-07-06 14:19:53 +0800

    Add remark request frontend and backend logic

--- backend/routes/lecturer.php
+++ backend/routes/lecturer.php
@@ -6,6 +6,7 @@ use App\Controllers\LecturerController;
 
 return function (App $app) {
     $app->group('/api/lecturer', function ($group) {
+        $group->get('/profile', [\App\Controllers\LecturerController::class, 'getProfile']);
         $group->get('/courses/{course_id}/students', LecturerController::class . ':getCourseStudents');
         $group->post('/courses/{course_id}/students', LecturerController::class . ':addStudentToCourse');
         $group->delete('/courses/{course_id}/students/{student_id}', LecturerController::class . ':removeStudentFromCourse');
@@ -20,5 +21,8 @@ return function (App $app) {
         $group->get('/courses/{course_id}/marks', LecturerController::class . ':getCourseMarks');
         $group->post('/courses/{course_id}/marks', LecturerController::class . ':saveCourseMarks');
         $group->post('/courses/{course_id}/marks/upload', LecturerController::class . ':uploadMarksCsv');
+        $group->get('/courses/{course_id}/analytics', LecturerController::class . ':getCourseAnalytics');
+        $group->get('/remark-requests', LecturerController::class . ':getRemarkRequests');
+        $group->put('/remark-requests/{id}', LecturerController::class . ':updateRemarkRequest');
     });
 };
