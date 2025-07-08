9d53d96966610c693143b1d7750cfc15a533f658
nurizzatie <nurizzati.jsl@gmail.com>
2025-07-08 19:18:21 +0800

    Add advisee progress details

--- frontend/src/components/advisor/StudentList.vue
+++ frontend/src/components/advisor/StudentList.vue
@@ -11,6 +11,13 @@
         Assigned Students
       </div>
       <div class="card-body table-responsive">
+        <!-- Search Bar -->
+        <div class="mb-3 input-group">
+          <span class="input-group-text bg-light">
+            <i class="fas fa-search"></i>
+          </span>
+          <input v-model="searchQuery" class="form-control" placeholder="Search by name or matric no" />
+        </div>
         <table class="table table-bordered table-hover text-center align-middle">
           <thead class="table-dark">
             <tr>
@@ -18,17 +25,23 @@
               <th>Name</th>
               <th>Email</th>
               <th>Matric Number</th>
+              <th>Action</th>
             </tr>
           </thead>
           <tbody>
             <tr v-if="!students.length">
               <td colspan="4" class="text-muted text-center py-3">No students found.</td>
             </tr>
-            <tr v-for="(student, index) in students" :key="student.id">
+            <tr v-for="(student, index) in filteredStudents" :key="student.id">
               <td>{{ index + 1 }}</td>
               <td>{{ student.name }}</td>
               <td>{{ student.email }}</td>
               <td>{{ student.matric_number }}</td>
+              <td>
+                <div>
+                  <router-link :to="`/advisor/advisee/${student.id}/progress`" class="btn btn-sm btn-dark">View</router-link>
+                </div>
+              </td>
             </tr>
           </tbody>
         </table>
@@ -45,8 +58,19 @@ export default {
   data() {
     return {
       user: JSON.parse(localStorage.getItem('user')),
-      students: []
+      students: [],
+      searchQuery: '',
     };
+  },
+  computed: {
+    filteredStudents() {
+      const q = this.searchQuery.toLowerCase();
+      return this.students.filter(
+        s =>
+          s.name.toLowerCase().includes(q) ||
+          s.matric_number.toLowerCase().includes(q)
+      );
+    }
   },
   methods: {
     async fetchStudents() {
