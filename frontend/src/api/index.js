199592cee5f59c906acc94ab2fbf018b9dc9539d
nurizzatie <nurizzati.jsl@gmail.com>
2025-07-15 17:44:05 +0800

    change api setup in index.js

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,12 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api', // Slim base
-  
+  baseURL: import.meta.env.VITE_API_URL + '/api', // base URL from .env + /api
   headers: {
     'Content-Type': 'application/json'
   }
-  
 });
 
 export default api;
